d588f90c500e791855a57bdb8bebec74a32739fa
MaciejKwiecinski <mackwiecinski@gmail.com>
2019-08-27 23:20:48 +0200

    xD

--- Library/models.py
+++ Library/models.py
@@ -1,19 +1,26 @@
 from django.db import models
-from .validators import MinYearValidator, MaxYearValidator,MinPagesValidator
+from .validators import MinYearValidator, MaxYearValidator,MinPagesValidator,ISBN10Validator,ISBN13Validator
 
 # Create your models here.
 ISBN = [(0,'ISBN-10'),
         (1,'ISBN-13')]
 
+class Authors(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Identyfires(models.Model):
     type = models.IntegerField()
-    identifier = models.IntegerField()
+    identifier = models.IntegerField(validators=[ISBN10Validator,ISBN13Validator])
 
 class BookInfo(models.Model):
-    title = models.TextField
-    authors = models.TextField()
+    title = models.TextField()
+    authors = models.ForeignKey(Authors,on_delete=models.CASCADE)
     publishedDate = models.IntegerField(validators=[MinYearValidator,MaxYearValidator])
     industryIdentifiers = models.ForeignKey(Identyfires,on_delete=models.CASCADE)
     pageCount=models.IntegerField(validators=[MinPagesValidator])
     imageLinks = models.TextField()
     language = models.TextField()
+
